6b68aa0e8a065b81e29008ad83349abf112be8b6
AmyJane <amyjane.reilly@gmail.com>
2019-07-19 14:09:21 -0400

    I hope this works :)

--- src/theme.js
+++ src/theme.js
@@ -1,22 +1,5 @@
-import WebFont from "webfontloader";
-
-WebFont.load({
-  google: {
-    families: ["Source Sans Pro:400", "sans-serif"]
-  }
-});
-
-const theme = {
-  colors: {
-    devLogo: "#FC6264",
-    mainBackground: "#FFF"
-  },
-  fonts: {
-    primary: {
-      family: "Source Sans Pro, sans-serif",
-      weight: 400
-    }
-  }
-};
+const theme ={
+    color: SVGFEGaussianBlurElement
+}
 
 export default theme;
